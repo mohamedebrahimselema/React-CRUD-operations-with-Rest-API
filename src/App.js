e2e5eed4b133a5d6b1bfc8a780152f229fc9daa1
mohamedibrahemselema <mohamed.ibrahiem5905@ci.menofia.edu.eg>
2020-11-25 01:11:35 +0200

    home screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import logo from './logo.svg';
-import NavBar from './components/NavBar'
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import NavBar from './components/NavBar';
+import HomeScreen from './screens/HomeScreen';
+import AddScreen from './screens/AddScreen';
+import EditScreen from './screens/EditScreen';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
-    return ( 
-    <div className = "App">
-        <NavBar />
-
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <NavBar />
+          <Switch>            
+            <Route path="/add" component={AddScreen} />
+            <Route path="/edit/:id" component={EditScreen} />
+            <Route path="/" component={HomeScreen} />
+          </Switch>
         </div>
-    );
+      </Router>
+    </div>
+  );
 }
+
 
 export default App;
